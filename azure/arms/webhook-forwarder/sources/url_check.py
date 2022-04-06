95b0750405c37d619a7d1c462a023fcddf485516
Ben Thomasson <bthomass@redhat.com>
2022-04-06 09:56:52 -0400

    Adding verify state

--- azure/arms/webhook-forwarder/sources/url_check.py
+++ azure/arms/webhook-forwarder/sources/url_check.py
@@ -1,5 +1,3 @@
-
-
 import requests
 import time
 from requests.exceptions import ConnectionError
@@ -7,8 +5,8 @@ from requests.exceptions import ConnectionError
 
 def main(queue, args):
 
-    urls = args.get('urls', [])
-    delay = args.get('delay', 1)
+    urls = args.get("urls", [])
+    delay = args.get("delay", 1)
 
     if not urls:
         return
@@ -17,25 +15,32 @@ def main(queue, args):
 
         for url in urls:
             try:
-                response = requests.get(url)
-                queue.put(dict(url_check=dict(url=url,
-                                              status='up' if response.status_code == 200 else 'down',
-                                              status_code=response.status_code)))
+                response = requests.get(url, timeout=10, verify=False)
+                queue.put(
+                    dict(
+                        url_check=dict(
+                            url=url,
+                            status="up" if response.status_code == 200 else "down",
+                            status_code=response.status_code,
+                        ),
+                        meta=dict(time=time.time()),
+                    )
+                )
 
             except ConnectionError:
-                queue.put(dict(url_check=dict(url=url,
-                                              status='down',
-                                              status_code=None)))
+                queue.put(
+                    dict(
+                        url_check=dict(url=url, status="down", status_code=None),
+                        meta=dict(time=time.time()),
+                    )
+                )
         time.sleep(delay)
 
 
-
-
 if __name__ == "__main__":
+
     class MockQueue:
         def put(self, event):
             print(event)
 
-    main(MockQueue(), {'urls': ['http://redhat.com']})
-
-
+    main(MockQueue(), {"urls": ["http://redhat.com"]})
